e91808ba0135ec8a39c7b8ff5eb1cb7962fd002b
Jakob <j.respondek@qvest-digital.com>
2024-11-21 16:05:53 +0100

    app: refactor logging

--- bookshelf_app/lib/shared/logger.dart
+++ bookshelf_app/lib/shared/logger.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/foundation.dart';
 
 mixin Logger {
+  static DateTime get now => DateTime.now();
+
   static void log(String message, {String? operation}) {
     // here we could also add log to Crashlytics, Sentry, etc.
     if (kDebugMode) {
-      print(operation != null ? '$operation: $message' : message);
+      final log = {
+        'level': 'INFO',
+        'timestamp': now,
+        'operation': operation,
+        'message': message,
+      };
+      print(log);
     }
   }
 
   static void logError(Object error, {String? operation}) {
     // here we could also add log to Crashlytics, Sentry, etc.
     if (kDebugMode) {
-      print(operation != null ? '$operation: $error' : error);
+      final log = {
+        'level': 'ERROR',
+        'timestamp': now,
+        'operation': operation,
+        'error': error.toString(),
+      };
+      print(log);
     }
   }
 }
